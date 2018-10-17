4afbbfc6eaa9c535eb408a9b509eee1698cd0a09
VERMOUTH\HP <oeidonny.winarto@gmail.com>
2018-10-17 15:48:40 +0700

    tampilan notification dari md ke xs

--- resources/views/backend/elements/notification.blade.php
+++ resources/views/backend/elements/notification.blade.php
@@ -1,6 +1,6 @@
 @if (Session::has('success'))
     <div class="row">
-	    <div class="col-md-12 alert alert-<?=Session::get('mode');?> alert-dismissible" role="alert">
+	    <div class="col-xs-12 alert alert-<?=Session::get('mode');?> alert-dismissible" role="alert">
 		    {{ Session::get('success') }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         </div>
     </div>
